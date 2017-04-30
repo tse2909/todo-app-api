e1195e5cbee1b603ef64f081af1a3b039313b603
Maryanto Tse <maryanto.tse@gmail.com>
2017-05-01 01:28:54 +0700

    add

--- index.js
+++ index.js
@@ -15,35 +15,45 @@ var WooCommerce = new WooCommerceAPI({
 
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
-app.get('/getProducts', function(request,response) {
+app.get('/getProducts', function (request, response) {
 
   WooCommerce.get('products', function (err, data, res) {
-    if (err){
+    if (err) {
       response.send(err)
-    } else{
+    } else {
       response.json(data)
     }
   });
 
 });
 
+app.post('postOrders', function (request, response) {
+  WooCommerce.post('orders', data, function (err, data, res) {
+    console.log(res);
+    if (err) {
+      response.send(err)
+    } else {
+      response.json(data)
+    }
+  });
+})
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 8080, function () {
   console.log("Listening")
 });
