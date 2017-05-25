3163cf28ec0ab7d60c5b85adc975233f39ef13ac
Maryanto Tse <maryanto.tse@gmail.com>
2017-05-25 23:18:03 +0700

    orders

--- index.js
+++ index.js
@@ -60,8 +60,43 @@ app.get('/getProducts', function (request, response) {
 
 });
 
+var data = {
+  payment_method: 'bacs',
+  payment_method_title: 'Direct Bank Transfer',
+  set_paid: true,
+  billing: {
+    first_name: 'John',
+    last_name: 'Doe',
+    address_1: '969 Market',
+    address_2: '',
+    city: 'San Francisco',
+    state: 'CA',
+    postcode: '94103',
+    country: 'US',
+    email: 'john.doe@example.com',
+    phone: '(555) 555-5555'
+  },
+  shipping: {
+    first_name: 'John',
+    last_name: 'Doe',
+    address_1: '969 Market',
+    address_2: '',
+    city: 'San Francisco',
+    state: 'CA',
+    postcode: '94103',
+    country: 'US'
+  },
+  line_items: [
+    {
+      product_id: 64,
+      quantity: 2
+    },
+    
+  ]
+};
+
 app.post('/postOrders', function (request, response) {
-  WooCommerce.post('orders', request.body.data, function (err, data, res) {
+  WooCommerce.post('orders', data, function (err, data, res) {
     console.log(res);
     if (err) {
       response.send(err)
