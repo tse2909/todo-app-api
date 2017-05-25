d142c3da2fac669f41d3e5251868371b5b4175a7
Maryanto Tse <maryanto.tse@gmail.com>
2017-05-25 23:46:30 +0700

    orders

--- index.js
+++ index.js
@@ -49,50 +49,9 @@ app.get('/getProducts', function (request, response) {
 
 });
 
-var data = {
-  payment_method: 'bacs',
-  payment_method_title: 'Direct Bank Transfer',
-  set_paid: true,
-  billing: {
-    
-    city: 'San Francisco',
-    state: 'CA',
-    postcode: '94103',
-    country: 'US',
-    email: 'john.doe@example.com',
-    phone: '(555) 555-5555'
-  },
-  shipping: {
-    first_name: 'John',
-    last_name: 'Doe',
-    address_1: '969 Market',
-    address_2: '',
-    city: 'San Francisco',
-    state: 'CA',
-    postcode: '94103',
-    country: 'US',
-    email: 'john.doe@example.com',
-    phone: '(555) 555-5555'
-  },
-  line_items: [
-    {
-      product_id: 64,
-      quantity: 2
-    },
-    
-  ],
-
-    shipping_lines: [
-    {
-      method_id: 'flat_rate',
-      method_title: 'Flat Rate',
-      total: 10
-    }
-  ]
-};
 
 app.post('/postOrders', function (request, response) {
-  WooCommerce.post('orders', data, function (err, data, res) {
+  WooCommerce.post('orders', request.body.data, function (err, data, res) {
     console.log(res);
     if (err) {
       response.send(err)
